79dc40ebfefff7edf0bf707f2d2e0627493572f1
cesarberci <cesar@berci.com.br>
2021-10-08 12:42:36 -0300

    first files

---
+++ hw_config.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   hw_config.h
+ * Author: cesar
+ *
+ * Created on October 1, 2021, 12:37 PM
+ */
+
+#ifndef HW_CONFIG_H
+#define	HW_CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HW_CONFIG_H */
+
